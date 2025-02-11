d981750b154e1aad773f5690a08048e7ea979726
JAMES M <168496401+princelamar509@users.noreply.github.com>
2025-02-11 12:43:25 -0500

    hashR

--- securepay-demo/src/App.jsx
+++ securepay-demo/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import BottomNav from './components/BottomNav';
 import HomeView from './components/HomeView';
